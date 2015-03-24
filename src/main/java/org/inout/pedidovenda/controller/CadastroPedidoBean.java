5ffe58755347eb31ffc4063977eaa6a48de48c88
RobsonMattos <rjnmattos@gmail.com>
2015-03-23 22:00:34 -0300

    Validação da tela de pedidos

--- src/main/java/org/inout/pedidovenda/controller/CadastroPedidoBean.java
+++ src/main/java/org/inout/pedidovenda/controller/CadastroPedidoBean.java
@@ -1,20 +1,37 @@
 package org.inout.pedidovenda.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
-@Named
-@RequestScoped
-public class CadastroPedidoBean {
+import org.inout.pedidovenda.model.EnderecoEntrega;
+import org.inout.pedidovenda.model.Pedido;
 
+@Named
+@ViewScoped
+public class CadastroPedidoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Pedido pedido;
 	private List<Integer> itens;
 	
 	public CadastroPedidoBean() {
+		pedido = new Pedido();
+		pedido.setEnderecoEntrega(new EnderecoEntrega());
 		itens = new ArrayList<>();
 		itens.add(1);
+	}
+
+	public void salvar() {
+		
+	}
+	
+	public Pedido getPedido() {
+		return pedido;
 	}
 
 	public List<Integer> getItens() {
