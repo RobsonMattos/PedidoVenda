cac06924a38addfee9d34cc400e38414d18d9886
RobsonMattos <rjnmattos@gmail.com>
2015-03-26 22:54:00 -0300

    Carga, EntiyManagerProducer, consulta cotegorias.

--- src/main/java/org/inout/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/org/inout/pedidovenda/controller/CadastroProdutoBean.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
 import org.inout.pedidovenda.model.Categoria;
 import org.inout.pedidovenda.model.Produto;
@@ -15,12 +17,19 @@ public class CadastroProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Inject
+	private EntityManager manager;
+	
 	private Produto produto;
 
 	private List<Categoria> categoriasRaizes;
 	
 	public CadastroProdutoBean() {
 		produto = new Produto();
+	}
+	
+	public void inicializar() {
+		categoriasRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
 	}
 	
 	public void salvar() {
